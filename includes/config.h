0b0f8460c66aa63ac930458a2e1cd09d0e326cf7
x7l7j8cc <x7l7j8cc@gmail.com>
2020-04-19 17:15:35 +0200

    0.1.4-mod

--- includes/config.h
+++ includes/config.h
@@ -10,13 +10,14 @@
 
 #define APP_NAME                "Kosmos Sigpatch-Updater"
 #define APP_VERSION             "0.1.4 Mod"
-#define CURSOR_LIST_MAX         4 //On-SCREEN OPTION LIST -1
+#define CURSOR_LIST_MAX         5 //On-SCREEN OPTION LIST -1
 
 #define UP_MSIG       0
 #define UP_FSIG       1
 #define UP_HSIG       2
 #define UP_RMNL       3
-#define UP_APP        4
+#define UP_RMPT       4
+#define UP_APP        5
 
 
 //Link to DL will AutoUpdate files from Server side.
@@ -24,20 +25,28 @@
 #define SIG_FULL_URL    "https://github.com/x7l7j8cc/ksigpatch-updater/releases/download/v0.1.4-mod/latest-Kosmos-Patches_v15.5_byJoonie86-FULL.zip"
 #define SIG_HARK1_URL   "https://github.com/HarukoNX/Atmosphere/releases/latest/download/loader.kip"
 #define SIG_HARK2_URL   "https://github.com/HarukoNX/Atmosphere/releases/latest/download/patches.ini"
+#define SIG_HEKATE_PTC  "https://github.com/x7l7j8cc/ksigpatch-updater/releases/latest/download/sigpatch_hekate_ipl.ini"
+#define SIG_REMOVE_URL  "https://github.com/x7l7j8cc/ksigpatch-updater/releases/latest/download/default_kosmos_config.zip"
 #define APP_URL         "https://github.com/x7l7j8cc/ksigpatch-updater/releases/latest/download/ksigpatch-updater.nro"
 
 //Temp File to remove, and patch file for tinfoil (if found)
 #define DIR_TINFOIL_PATCH  "/atmosphere/kips/ams_mitm.kip"
-#define DIR_FILE_KIP       "/atmosphere/kips/loader.kip"
+#define DIR_FILE_LKIP      "/atmosphere/kips/loader.kip"
+#define DIR_FILE_HEKATE    "/bootloader/hekate_ipl.ini"
 #define DIR_FILE_PATCHES   "/bootloader/patches.ini"
 #define DIR_NICHOLELOGO    "/atmosphere/exefs_patches/nichole_logo"
+#define DIR_RMV_ESPATCH    "/atmosphere/exefs_patches/es_patches"
+#define DIR_RMV_KIPS       "/atmosphere/kips"
 #define TEMP_FILE          "/switch/ksigpatch-updater/temp"
 
-#define LOPTION_1       "Update Kosmos (Mini) Sigpatches (ES/FS Only)"
-#define LIOPTION_1       "Install Kosmos (Mini) Sigpatches (ES/FS Only)"
-#define LOPTION_2       "Update Kosmos (Full) Sigpatches (+ACID for Tinfoil Support)"
-#define LIOPTION_2       "Install Kosmos (Full) Sigpatches (+ACID for Tinfoil Support)"
-#define LOPTION_3       "Update Sigpatches by HarukoNX"
-#define LIOPTION_3       "Install Sigpatches by HarukoNX"
+//Menu Options
+#define LOPTION_1       "Install Kosmos (Mini) Sigpatches (ES/FS Only)"
+#define LOPTION_2       "Install Kosmos (Full) Sigpatches (for Tinfoil Support)"
+#define LOPTION_3       "Install Sigpatches by HarukoNX"
 #define LOPTION_4       "Remove Nichole-Logo from Kosmos Boot"
-#define LOPTION_5       "Update this App"
+#define LOPTION_5       "Remove SigPatches from Kosmos"
+#define LOPTION_6       "Update this App"
+
+//#define LIOPTION_1      "Update Kosmos (Mini) Sigpatches (ES/FS Only)"
+//#define LIOPTION_2      "Update Kosmos (Full) Sigpatches (for Tinfoil Support)"
+//#define LIOPTION_3      "Update Sigpatches by HarukoNX"
--- source/main.c
+++ source/main.c
@@ -12,29 +12,26 @@
 
 
 const char *OPTION_LIST[] = {
-    LOPTION_1 ,LOPTION_2, LOPTION_3, LOPTION_4, LOPTION_5
+    LOPTION_1 ,LOPTION_2, LOPTION_3, LOPTION_4, LOPTION_5, LOPTION_6
 };
-const char *IOPTION_LIST[] = {
-    LIOPTION_1 ,LIOPTION_2, LIOPTION_3, LOPTION_4, LOPTION_5
-};
+short cursor = 0;
+//const char *IOPTION_LIST[] = { LIOPTION_1 ,LIOPTION_2, LIOPTION_3, LOPTION_4, LOPTION_5, LOPTION_6 };
 
-/**/
-long int findSize(char file_name[]) 
-{ 
-    FILE* fp = fopen(file_name, "r"); 
-  
-    if (fp == NULL) { 
-        printf("File Not Found!\n"); 
-        return -1; 
-    } 
-  
-    fseek(fp, 0L, SEEK_END); 
-    long int res = ftell(fp); 
-    fclose(fp); 
-  
-    return res; 
+
+void removeFolder(const char *fld)
+{
+    DIR *theFolder = opendir(fld);
+    struct dirent *next_file;
+    char filepath[380];
+
+    while ( (next_file = readdir(theFolder)) != NULL )
+    {
+        sprintf(filepath, "%s/%s", fld, next_file->d_name);
+        remove(filepath);
+    }
+    closedir(theFolder);
+    rmdir(fld);
 }
-/**/
 
 void refreshScreen(int cursor)
 {
@@ -44,41 +41,23 @@ void refreshScreen(int cursor)
     changeTColor("cyan"); printf("\n%s: v%s.\n", APP_NAME, APP_VERSION);
     changeTColor("red"); printf("Original Code by %s, modded for Kosmos Only by %s\n\n", ORIG_AUTHOR, MODK_AUTHOR);
     
-    char file_name[] = { DIR_FILE_PATCHES }; 
-    long int res = findSize(file_name); 
-    //if (res != -1) printf("Size of the file is %ld bytes \n", res);
-    //11552
-    
     //Change to White Text
     changeTColor("white"); printf("\nPress (UP/DOWN) to navigate and (A) to select option\nPress (+) to exit\n\n\n");
 
+    changeTColor("magenta"); printf("################################################\n\n");
     changeTColor("green"); //Change to Green Text
     for (int i = 0; i < CURSOR_LIST_MAX + 1; i++)
     {
-        if( i == 2 ){
-            changeTColor("cyan");
-        } else if( i == 3 ){
-            changeTColor("white");
-        } else if( i == 4 ){
-            changeTColor("red");
+        if( i == 2 ){ changeTColor("cyan");
+        } else if( i == 3 || i == 4 ){ changeTColor("white");
+        } else if( i == 5 ){ changeTColor("red");
         } else {
             changeTColor("green");
         }
         
-        if( access( DIR_FILE_PATCHES, F_OK ) != -1 ) {
-            // file exists
-            if( i==2 && res == 11388 ){
-                printf("[%c] %s\n\n", cursor == i ? '>' : ' ', OPTION_LIST[i]);
-            } else if( i==2 && res != 11388 ){
-                printf("[%c] %s\n\n", cursor == i ? '>' : ' ', IOPTION_LIST[i]);
-            } else {
-                printf("[%c] %s\n\n", cursor == i ? '>' : ' ', OPTION_LIST[i]);
-            }
-        } else {
-            // file doesn't exist
-            printf("[%c] %s\n\n", cursor == i ? '>' : ' ', IOPTION_LIST[i]);
-        }
+        printf("[%c] %s\n\n", cursor == i ? '>' : ' ', OPTION_LIST[i]);
     }
+    changeTColor("magenta"); printf("################################################\n\n");
     
     consoleUpdate(NULL);
 }
@@ -102,6 +81,7 @@ int appInit()
 void appRefresh()
 {
     appInit();
+    cursor = 0;
     refreshScreen(0);
 }
 
@@ -138,6 +118,15 @@ void rmvNLOGEnd()
     svcSleepThread(3000000000ull);
     appRefresh();
 }
+void rmvSigPtcEnd()
+{
+    changeTColor("magenta"); //Change to Magenta Text
+    printf("\nSigPatches Removed!!!\n");
+    printf("Please Wait 3 Second to Reload Menu.\n");
+    consoleUpdate(NULL);
+    svcSleepThread(3000000000ull);
+    appRefresh();
+}
 
 int main(int argc, char **argv)
 {
@@ -149,7 +138,7 @@ int main(int argc, char **argv)
     chdir(ROOT);
 
     // set the cursor position to 0
-    short cursor = 0;
+    cursor = 0;
 
     // main menu
     refreshScreen(cursor);
@@ -211,12 +200,14 @@ int main(int argc, char **argv)
                 break;
                 
             case UP_HSIG:
+                changeTColor("red"); printf("creating kips folder...\n"); mkdir(DIR_RMV_KIPS, 0777);
+                changeTColor("yellow");
                 if (downloadFile(SIG_HARK1_URL, TEMP_FILE, OFF))
                 {
                     //unzip(TEMP_FILE);
                     changeTColor("red");
                     printf("moving loader file...\n");
-                    remove(DIR_FILE_KIP); rename(TEMP_FILE, DIR_FILE_KIP);
+                    remove(DIR_FILE_LKIP); rename(TEMP_FILE, DIR_FILE_LKIP);
                     printf("removing temp file...\n");
                     remove(TEMP_FILE);
                     
@@ -229,11 +220,26 @@ int main(int argc, char **argv)
                         remove(DIR_FILE_PATCHES); rename(TEMP_FILE, DIR_FILE_PATCHES);
                         printf("removing temp file...\n");
                         remove(TEMP_FILE);
-                        kosmosUpdEnd();
                     }
                     else
                     {
                         printDisplay("Failed to download sigpatches by HarukoNX-patchini patch\n");
+                    }
+                    
+                    changeTColor("yellow");
+                    if (downloadFile(SIG_HEKATE_PTC, TEMP_FILE, OFF))
+                    {
+                        //unzip(TEMP_FILE);
+                        changeTColor("red");
+                        printf("moving hekate-ipl file...\n");
+                        remove(DIR_FILE_HEKATE); rename(TEMP_FILE, DIR_FILE_HEKATE);
+                        printf("removing temp file...\n");
+                        remove(TEMP_FILE);
+                        kosmosUpdEnd();
+                    }
+                    else
+                    {
+                        printDisplay("Failed to download hekate-ipl\n");
                     }
                 }
                 else
@@ -244,18 +250,29 @@ int main(int argc, char **argv)
                 
             case UP_RMNL:
                 changeTColor("red"); printf("removing nichole logo...\n");
-                DIR *theFolder = opendir(DIR_NICHOLELOGO);
-                struct dirent *next_file;
-                char filepath[380];
-
-                while ( (next_file = readdir(theFolder)) != NULL )
-                {
-                    sprintf(filepath, "%s/%s", DIR_NICHOLELOGO, next_file->d_name);
-                    remove(filepath);
-                }
-                closedir(theFolder);
-                rmdir(DIR_NICHOLELOGO);
+                removeFolder(DIR_NICHOLELOGO);
                 rmvNLOGEnd();
+                break;
+                
+            case UP_RMPT:
+                changeTColor("red"); printf("removing sigpatches from kosmos...\n");
+                removeFolder(DIR_RMV_ESPATCH);
+                removeFolder(DIR_RMV_KIPS);
+                remove(DIR_FILE_PATCHES);
+                
+                changeTColor("yellow");
+                if (downloadFile(SIG_REMOVE_URL, TEMP_FILE, OFF))
+                {
+                    unzip(TEMP_FILE);
+                    changeTColor("red");
+                    printf("removing temp file...\n");
+                    remove(TEMP_FILE);
+                    rmvSigPtcEnd();
+                }
+                else
+                {
+                    printDisplay("Failed to download hekate default config\n");
+                }
                 break;
 
             case UP_APP:
